868744c9abd29e83c3ebc8ec651f6de26bec39f6
Dave Seidman <bullcitydave@gmail.com>
2014-07-14 20:22:26 -0400

    Two views but no working click events

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,10 @@
 
     var DoMe = Backbone.Model.extend({
 
-      // initialize: function() {
-      //    this.listenTo(this.model, 'destroy', this.remove);
-      // },
-
       defaults: function(){
           return {
             summary: 'Something I have to do',
-            details: 'Need some details',
+            details: '',
             dueDate: '',
             status: 'open'
         };
@@ -18,45 +14,58 @@
       urlRoot:'http://tiny-pizza-server.herokuapp.com/collections/dome',
 
       idAttribute: '_id'
+
     });
 
 
 /// DEFINE MODEL VIEW ///
 
     var DoMeView = Backbone.View.extend({
-      className : 'do-me-list',
+      className : 'do-me-item',
 
       initialize: function(){
-          console.log("Ready to do me!");
+        console.log("Ready to do me!");
         },
 
-        events: {
-        "click .edit" : "editToDo"
+      events: {
+        'click .edit'     : 'editToDo',
+        'click .complete' : 'completeToDo'
       },
 
-        editToDo: function (e) {
-              // $(this).children('strong').css('color', 'blue');
-              // $(this).children('strong').attr({'contenteditable': 'true'});
-              var parent = e.currentTarget.parentElement;
-              $(e.currentTarget.parentElement).find('strong').css('color','blue');
-              console.log('Editing');
+      completeToDo : function(e) {
+          alert('Completing');
+          console.log('Completing');
+        },
 
-            }
+
+      editToDo: function (e) {
+        console.log('It works!');
+        var parent = e.currentTarget.parentElement;
+        $(e.currentTarget.parentElement).find('strong').css('color','blue');
+        $(e.currentTarget.parentElement).find('strong').attr({'contenteditable':'true'});
+        console.log('Editing');
+        }
 
     });
 
-
+//// this doesn't do anything outside of a function
+$('.delete').click(function() {
+  alert('Deleting');
+  console.log('Delete');
+    var getId = ($(this).parent().attr('id'));
+    // doMeList.remove( doMeList.get(getId) );
+    var modo = doMeList.get(getId);
+    modo.destroy();
+})
 // This is a completely ridiculously way to force a sort that returns the latest submission first; converts _id from hexadecimal to decimal and sorts by negative, so higher _id values are shown first; since no _id has yet been generated for the new item, force a very large negative number to be returned
     var mostRecent = function (model) {
       var pseudoID;
       if (model.get('_id') == undefined) {
-        pseudoID = -9.5923158918808516e+28;
-        console.log('here');
+        pseudoID = -9e+28;
       }
       else {
         pseudoID = -(parseInt((model.get('_id')), 16));
       }
-      console.log(pseudoID);
       return pseudoID;
     }
 
@@ -71,7 +80,6 @@
 
 
     var doMeList = new DoMeList();
-
 
 
 /// DEFINE COLLECTION VIEW ///
@@ -92,16 +100,7 @@
           var rendered = template({doMeList: this.collection.toJSON()});
           this.$el.html(rendered);
           return this;
-        },
-
-        editToDo: function (e) {
-              // $(this).children('strong').css('color', 'blue');
-              // $(this).children('strong').attr({'contenteditable': 'true'});
-              var parent = e.currentTarget.parentElement;
-              $(e.currentTarget.parentElement).find('strong').css('color','blue');
-              console.log('Editing');
-
-            }
+        }
 
     });
 
@@ -120,6 +119,7 @@ $(document).ready(function() {
         return false;
       });
 
+
 ///this works first time only
     $('.delete').click(function() {
       alert('Deleting');
@@ -135,15 +135,16 @@ $(document).ready(function() {
       console.log('Completing');
 
     })
-    // $('.edit').click(function() {
-    //   alert('Editing');
-    //   alert($(this).siblings('strong').attr('contenteditable'));
-    //   $(this).siblings('strong').css('color', 'blue');
-    //   $(this).siblings('strong').attr({'contenteditable' : 'true'});
-    //   alert($(this).siblings('strong').attr('contenteditable'));
-    //   console.log('Editing');
-    //
-    // })
+    var editToDo = function() {
+      console.log('Editing');
+      $(this).siblings('strong').css('color', 'blue');
+      $(this).siblings('strong').attr({'contenteditable': 'true'});
+      $(this).siblings('.save').show();
+      $(this).hide();
+
+    }
+
+    $('.edit').click(editToDo);
 
  })
 
@@ -174,13 +175,18 @@ $('h1').click(function() {
         modo.destroy();
     })
 
-    // 
-    // $('.edit').click(editToDo);
-    //
-    //
-    // var editToDo = function() {
-    //   $(this).siblings('strong').css('color', 'blue');
-    //   $(this).siblings('strong').attr({'contenteditable': 'true'});
-    //   $(this).siblings('save').css('display', 'inline-block');
-    //   console.log('Editing');
-    // }
+
+
+
+
+
+    var editToDo = function() {
+      console.log('Editing');
+      $(this).siblings('strong').css('color', 'blue');
+      $(this).siblings('strong').attr({'contenteditable': 'true'});
+      $(this).siblings('.save').show();
+      $(this).hide();
+
+    }
+
+    $('.edit').click(editToDo);
