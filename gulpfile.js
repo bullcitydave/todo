8c02edfa8790b09233a0b6df9cced72c630cc9a4
Dave Seidman <bullcitydave@gmail.com>
2014-08-18 12:29:03 -0400

    Cleaned up variable names

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('images', function () {
 });
 
 gulp.task('extras', function () {
-  return gulp.src(['app/*.*', '!app/*.html'], {dot: true})
+  return gulp.src(['app/*.*', 'app/styles/font-awesome/*', '!app/*.html'], {dot: true})
     .pipe(gulp.dest('dist'));
 });
 
